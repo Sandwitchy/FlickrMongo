6a48013726b820d929eed2892730d0cd29e284ff
thibault <thibaultnesti@gmail.com>
2019-11-07 10:26:05 +0100

    Mise en forme gallery

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once('bdd.php');
     <meta charset="utf-8">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="/style .css">
+    <link rel="stylesheet" href="./style.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script type="text/javascript">
     // MDB Lightbox Init
@@ -24,11 +24,13 @@ require_once('bdd.php');
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
       </form>
     </nav>
-    <div class="gallery" id="gallery">
+    <div class="col-md-12">
+      <div class="row">
+        <div class="gal">
 <?php
 if (isset($_REQUEST["searchtag"])) {
   $tag = $_REQUEST["searchtag"];
-  $url = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=28984d098e9946c2c42b87eac57a678b&tags=". $tag ."&min_upload_date=&max_upload_date=&safe_search=&format=json&nojsoncallback=1";
+  $url = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=828e660b4e4c9f0a931a9adbe60ba348&tags=". $tag ."&min_upload_date=&max_upload_date=&safe_search=&format=json&nojsoncallback=1";
  /* if(search($tag)){
     print('fdsfjksdfs');
   }else{
@@ -39,13 +41,13 @@ if (isset($_REQUEST["searchtag"])) {
   foreach ($result["photos"]["photo"] as $img) {
     $imgurl = "https://farm" . $img["farm"] . ".staticflickr.com/" . $img["server"] . "/" . $img["id"] . "_" . $img["secret"] . ".jpg";
     ?>
-      <div class="col-md-4">
-        <img class="img-fluid" src="<?php echo $imgurl; ?>" alt="Card image cap">
-      </div>
+        <img class="img-fluid" src="<?php echo $imgurl; ?>" alt="Image">
     <?php
   }
 }
 ?>
+</div>
+</div>
 </div>
 </body>
 </html>
